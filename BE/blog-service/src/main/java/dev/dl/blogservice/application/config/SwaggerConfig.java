b07e21301c7ba556cc10a80a135b0db529254eed
Lộc Võ Tuấn <tuanloc1105@gmail.com>
2022-08-15 14:52:11 +0700

    Update SwaggerConfig.java

--- BE/blog-service/src/main/java/dev/dl/blogservice/application/config/SwaggerConfig.java
+++ BE/blog-service/src/main/java/dev/dl/blogservice/application/config/SwaggerConfig.java
@@ -6,20 +6,14 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.Contact;
-import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger.web.DocExpansion;
 import springfox.documentation.swagger.web.ModelRendering;
 import springfox.documentation.swagger.web.OperationsSorter;
 import springfox.documentation.swagger.web.UiConfiguration;
 import springfox.documentation.swagger.web.UiConfigurationBuilder;
-
-import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
