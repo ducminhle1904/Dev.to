2e6c7ad3c9692c69b491e9599887928cde89bfb1
duclm <ducle7144@gmail.com>
2022-08-16 22:20:45 +0700

    update header

--- FE/header-app/src/root.component.tsx
+++ FE/header-app/src/root.component.tsx
@@ -1,28 +1,36 @@
-import { Layout, Menu } from "antd";
+import { AliwangwangFilled } from "@ant-design/icons";
+import { Layout, Input, Button } from "antd";
 import { BrowserRouter } from "react-router-dom";
-import { navigateToUrl } from "single-spa";
 
 const { Header } = Layout;
-const items = [
-    { label: "React app", key: "/react" }, // remember to pass the key prop
-    { label: "Angular app", key: "/angular" }, // which is required
-];
+const { Search } = Input;
 
 export default function Root(props: any) {
-    function onChangeRoute(event: any) {
-        return navigateToUrl(event.key);
-    }
     return (
         <BrowserRouter>
-            <Header style={{ position: "fixed", zIndex: 1, width: "100%", top: 0 }}>
-                <div className="logo" />
-                <Menu
-                    theme="dark"
-                    mode="horizontal"
-                    defaultSelectedKeys={["1"]}
-                    items={items}
-                    onClick={onChangeRoute}
-                />
+            <Header
+                style={{
+                    position: "fixed",
+                    zIndex: 1,
+                    width: "100%",
+                    top: 0,
+                    background: "#FFFFFF",
+                    boxShadow: "0 1px 1px rgba(0,0,0,0.1)",
+                }}
+            >
+                <div
+                    className="logo"
+                    style={{ display: "flex", alignItems: "center", height: "100%", justifyContent: "space-between" }}
+                >
+                    <div style={{ display: "flex", alignItems: "center", width: "50%" }}>
+                        <AliwangwangFilled style={{ fontSize: "50px", marginRight: "20px" }} />
+                        <Search placeholder="Search..." loading={false} style={{ maxWidth: "420px" }} size="large" />
+                    </div>
+                    <div>
+                        <Button style={{ marginRight: "20px" }}>Log in</Button>
+                        <Button type="primary">Create account</Button>
+                    </div>
+                </div>
             </Header>
         </BrowserRouter>
     );
--- FE/react-app/src/root.component.tsx
+++ FE/react-app/src/root.component.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import App from "./App";
 
 export default function Root(props) {
-  return (
-    <section style={{ marginTop: 100 }}>
-      <App name="Testapp" />
-    </section>
-  );
+    return (
+        <section style={{ marginTop: 50 }}>
+            <App name="Testapp" />
+        </section>
+    );
 }
