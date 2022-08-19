448449353dd92a824cf4abee882e1d9e62f5cfce
duclm <ducle7144@gmail.com>
2022-08-19 20:51:14 +0700

    Transfer to Astro

--- FE/footer-app/src/root.component.tsx
+++
@@ -1,6 +0,0 @@
-import { Layout } from "antd";
-
-const { Footer } = Layout;
-export default function Root(props: any) {
-    return <Footer style={{ textAlign: "center" }}>Ant Design ©2018 Created by Ant UED</Footer>;
-}
