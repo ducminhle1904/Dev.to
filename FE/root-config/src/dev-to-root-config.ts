f7bf207b5575630692f27b7f557d4698c3e86b0b
duclm <ducle7144@gmail.com>
2022-08-21 14:58:14 +0700

    Back to Single-spa

---
+++ FE/root-config/src/dev-to-root-config.ts
@@ -0,0 +1,24 @@
+import { registerApplication, start } from "single-spa";
+import { constructApplications, constructRoutes, constructLayoutEngine } from "single-spa-layout";
+import microfrontendLayout from "./microfrontend-layout.html";
+
+const routes = constructRoutes(microfrontendLayout, {
+    loaders: {
+        topNav: "<h1>Loading topnav</h1>",
+    },
+    errors: {
+        topNav: "<h1>Failed to load topnav</h1>",
+    },
+    props: {},
+});
+const applications = constructApplications({
+    routes,
+    loadApp({ name }) {
+        return System.import(name);
+    },
+});
+const layoutEngine = constructLayoutEngine({ routes, applications });
+
+applications.forEach(registerApplication);
+layoutEngine.activate();
+start();
